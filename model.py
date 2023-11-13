416c841c51e9eeaef8048a5ff213c91029f69b45
Benjamin Paul Jaeger <benjamin@picovoice.ai>
2023-11-13 11:20:48 -0800

    wip

--- model.py
+++ model.py
@@ -13,16 +13,16 @@ class SimpleCNN(Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
 
-        hidden_channels = 8
+        hidden_channels = 4
 
-        self.conv1 = Conv3d(in_channels=1, out_channels=hidden_channels, kernel_size=3, stride=1, padding=1)
-        self.conv2 = Conv3d(in_channels=hidden_channels, out_channels=hidden_channels, kernel_size=3, stride=1, padding=1)
+        self.conv1 = Conv3d(in_channels=1, out_channels=hidden_channels, kernel_size=7, stride=1, padding=1)
+        self.conv2 = Conv3d(in_channels=hidden_channels, out_channels=hidden_channels, kernel_size=5, stride=1, padding=1)
         self.conv3 = Conv3d(in_channels=hidden_channels, out_channels=hidden_channels, kernel_size=3, stride=1, padding=1)
 
         self.relu = ReLU()
         self.pool = MaxPool3d(kernel_size=2, stride=2)
 
-        self.dropout = Dropout(0.4)
+        self.dropout = Dropout(0.5)
 
         self.fc1 = Linear(32 * 32 * 5 * hidden_channels, 128)
         self.fc2 = Linear(128, 2)
@@ -36,6 +36,7 @@ class SimpleCNN(Module):
         x = self.pool(self.relu(self.conv2(x)))
         x = self.dropout(x)
         x = self.pool(self.relu(self.conv3(x)))
+
         x = self.dropout(x)
 
         x = flatten(x, 1)
